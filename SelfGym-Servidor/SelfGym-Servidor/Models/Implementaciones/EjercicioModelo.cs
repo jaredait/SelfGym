7bac93f316b288c5c54945592d55d11dd07dc96e
jaredait <jaredampudia@gmail.com>
2022-05-10 18:12:02 -0500

    Agregar modelos para obtener los ejercicios

---
+++ SelfGym-Servidor/SelfGym-Servidor/Models/Implementaciones/EjercicioModelo.cs
@@ -0,0 +1,55 @@
+﻿using SelfGym_Servidor.Models.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SelfGym_Servidor.Models.Implementaciones
+{
+    public class EjercicioModelo : IEjercicio
+    {
+        readonly private selfgymdbEntities _contexto;
+
+        public EjercicioModelo()
+        {
+            _contexto = new selfgymdbEntities();
+        }
+
+        public EJERCICIO ObtenerPorCodigo(string codigo)
+        {
+            try
+            {
+                return _contexto.EJERCICIO.Where(e => e.EJE_CODIGO == codigo).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public EJERCICIO ObtenerPorId(int id)
+        {
+            try
+            {
+                return _contexto.EJERCICIO.Where(e => e.EJE_ID == id).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable ObtenerTodos()
+        {
+            try
+            {
+                return _contexto.EJERCICIO.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SelfGym-Servidor/SelfGym-Servidor/Models/Interfaces/IEjercicio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfGym_Servidor.Models.Interfaces
+{
+    interface IEjercicio
+    {
+        IEnumerable ObtenerTodos();
+        EJERCICIO ObtenerPorId(int id);
+        EJERCICIO ObtenerPorCodigo(string codigo);
+    }
+}

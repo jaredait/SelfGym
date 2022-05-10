b84a9376ea5780cc9935af07a85ae0628a19f5b7
jaredait <jaredampudia@gmail.com>
2022-05-10 17:32:57 -0500

    Agregar script de datos

---
+++ ModelosDB/CodigoDB/Datos.sql
@@ -0,0 +1,5 @@
+INSERT INTO EJERCICIO VALUES('QR1', 'CURL DE BÍCEPS CON BARRA RECTA' , 'https://www.youtube.com/watch?v=oHO6dV7aQbE&feature=youtu.be');
+INSERT INTO EJERCICIO VALUES('QR2', 'FONDOS DE TRÍCEPS EN PARALELAS' , 'https://www.youtube.com/watch?v=1Vm1ATIi0AE&feature=youtu.be');
+INSERT INTO EJERCICIO VALUES('QR3', 'PRESS FRANCÉS' , 'https://www.youtube.com/watch?v=cGRkX8hD_cE&feature=youtu.be');
+INSERT INTO EJERCICIO VALUES('QR4', 'EXTENSIÓN DE TRÍCEPS EN POLEA' , 'https://www.youtube.com/watch?v=4dbi--ZG40Q&feature=youtu.be');
+INSERT INTO EJERCICIO VALUES('QR5', 'DOMINADAS CON AGARRE SUPINO' , 'https://www.youtube.com/watch?v=V59oKvWc9Ts&feature=youtu.be');

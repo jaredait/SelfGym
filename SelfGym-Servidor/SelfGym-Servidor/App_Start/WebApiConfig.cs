1c09afd9248b1c23d8c30998920dfcb1eeec4fb5
jaredait <jaredampudia@gmail.com>
2022-05-10 17:38:47 -0500

    Instalar CORS y registrar servicio

--- SelfGym-Servidor/SelfGym-Servidor/App_Start/WebApiConfig.cs
+++ SelfGym-Servidor/SelfGym-Servidor/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace SelfGym_Servidor
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 

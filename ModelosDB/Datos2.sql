f0e13f215aae9ceed423f912e67339059b39af92
jaredait <jaredampudia@gmail.com>
2022-06-03 17:03:52 -0500

    Generar nuevo diseño para la db

---
+++ ModelosDB/Datos2.sql
@@ -0,0 +1,19 @@
+INSERT INTO CATEGORIA VALUES('categoria1', 'Tren superior', 'Ejercicios para fortalecer el tren superior');
+INSERT INTO CATEGORIA VALUES('categoria2', 'Tren inferior', 'Ejercicios para fortalecer el tren inferior');
+INSERT INTO CATEGORIA VALUES('categoria3', 'Cardio', 'Ejercicios para generar resistencia');
+
+INSERT INTO EJERCICIO VALUES('ejercicio1', 'categoria1', 'Press de banca', 'Press de banca', 'url');
+INSERT INTO EJERCICIO VALUES('ejercicio2', 'categoria2', 'Sentadilla', 'Sentadilla', 'url');
+INSERT INTO EJERCICIO VALUES('ejercicio3', 'categoria3', 'Correr', 'Correr', 'url');
+
+INSERT INTO USUARIO VALUES('jaredait', 'f', 'jaredampudia@gmail.com', 24);
+INSERT INTO USUARIO VALUES('sofivm', 'f', 'jaredampudia@gmail.com', 24);
+INSERT INTO USUARIO VALUES('valvarez', 'f', 'jaredampudia@gmail.com', 24);
+INSERT INTO USUARIO VALUES('xcaceres', 'f', 'jaredampudia@gmail.com', 24);
+INSERT INTO USUARIO VALUES('aagarcia', 'f', 'jaredampudia@gmail.com', 24);
+
+INSERT INTO NOTICIA VALUES('noticia1', '<h1>Noticia 1<h1><p>Este es el contenido de la noticia 1</p>');
+INSERT INTO NOTICIA VALUES('noticia2', '<h1>Noticia 2<h1><p>Este es el contenido de la noticia 2</p>');
+INSERT INTO NOTICIA VALUES('noticia3', '<h1>Noticia 3<h1><p>Este es el contenido de la noticia 3</p>');
+
+
